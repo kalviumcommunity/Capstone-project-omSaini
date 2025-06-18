4a5d1ecf816ab0d540fb89ffc39339a06f388a22
omsaini12345 <om.saini.s70@kalvium.community>
2025-06-18 22:44:34 +0530

    addes

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -12,15 +12,15 @@ const { protect } = require('../middleware/auth');
 const router = express.Router();
 
 // Public routes
-// router.post('/register', register);
-// router.post('/login', login);
+router.post('/register', register);
+router.post('/login', login);
 
 
 // Protected routes
 router.get('/me', protect, getMe);
 router.put('/update-profile', protect, updateProfile);
 router.put('/change-password', protect, changePassword);
-// router.post('/logout', protect, logout);
+router.post('/logout', protect, logout);
 
 module.exports = router; 
 // update
